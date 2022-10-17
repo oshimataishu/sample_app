2d64b247603350930c8089cb23fa1428b9cea546
EC2 Default User <ec2-user@ip-172-31-94-93.ec2.internal>
2022-10-17 06:56:33 +0000

    [Finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,16 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
-
+  
+  def create
+    list = List.new(list_params)  #データを受け取り新規登録するためのインスタンス作成
+    
+    list.save  #データをデータベースに保存するためのsaveメソッドを実行
+    
+    redirect_to '/top'
+  end
+  
   def index
   end
 
@@ -10,4 +19,12 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private 
+  
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
+  get 'lists/new' #投稿ページの表示
+  post 'lists' => 'lists#create'
+  get 'lists/index' #一覧の表示
+  get 'lists/show' #内容の表示
+  get 'lists/edit' #内容の編集
   get '/top' => 'homes#top'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
